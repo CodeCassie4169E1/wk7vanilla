338dd70870c0251ef9a0203aedd67b0af15246b9
CodeCassie4169E1 <clipcassie@yahoo.com>
2023-09-11 19:23:22 -0500

    javascript update
    temp city description humidity and wind updated with axios and openweather

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemp(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "9ee44dd39b241c853f96295c78bd71cb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
